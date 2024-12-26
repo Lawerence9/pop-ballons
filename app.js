fb291f669eb79639853b7165a4e7dcdee8787972
Álvaro <82092265+Lawerence9@users.noreply.github.com>
2024-12-26 12:21:06 +0000

    First commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // we declare a new global variable containing an array that represents the ballons map
 // you have to add more colors into the ballonsMap array
-let ballonsMap = ['green'];
+let ballonsMap = ['green', 'yellow', 'pink', 'blue', 'red', 'black', 'orange', 'brown', 'yellow', 'red', 'grey', 'pink', 'orange', 'green', 'black', 'purple', 'blue', 'brown', 'pink', 'grey'];
 
 // poping a balloon is basically turning his color to null (no color)
 const popBalloon = (position) => {
     // set the color to null on the balloon position
+    ballonsMap[position] = null;
     render();
 }
 
@@ -12,12 +13,14 @@ const render = () => {
     
     // convert ballons map of colors into real html balloons
     const ballons = ballonsMap.map((color, position) => {
-        return `<div class="balloon active"></div>`; // <--- render each balloon
+        return `<div onclick=\"popBalloon(${position})\" class=\"balloon ${color === null ? 'popped' : ''}\" style=\"background: ${color || 'transparent'};\" ></div>`; // <--- render each balloon
     });
 
-    document.querySelector("#balloon-count").innerHTML = ballons.filter(b => b !== null).length; // <-- render the balloon count into the DOM
+    const activeBalloons = ballonsMap.filter(b => b !== null).length;
+    
+    document.querySelector("#balloon-count").innerHTML = activeBalloons; // <-- render the balloon count into the DOM
     document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
-
+    
     if(activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
 }
 
